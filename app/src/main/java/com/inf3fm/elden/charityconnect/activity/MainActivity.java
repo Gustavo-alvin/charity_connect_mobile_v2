3196d68f01a38489e3ac2911236618fc070700b9
Gustavo Alves da Silva <gustavoboygame@gmail.com>
2024-06-26 08:11:58 -0300

    first commit

---
+++ app/src/main/java/com/inf3fm/elden/charityconnect/activity/MainActivity.java
@@ -0,0 +1,73 @@
+package com.inf3fm.elden.charityconnect.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.inf3fm.elden.charityconnect.R;
+import com.inf3fm.elden.charityconnect.fragments.CatalogoFragment;
+import com.inf3fm.elden.charityconnect.fragments.HomeFragment;
+import com.inf3fm.elden.charityconnect.fragments.MenuFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton buttonHome, buttonCatalogo, buttonMenu;
+    private HomeFragment homeFragment;
+    private CatalogoFragment catalogoFragment;
+    private MenuFragment menuFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonHome = findViewById(R.id.buttonHome);
+        buttonCatalogo = findViewById(R.id.buttonCatalogo);
+        buttonMenu = findViewById(R.id.buttonMenu);
+
+        homeFragment = new HomeFragment();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameConteudo, homeFragment);
+        transaction.commit();
+
+        buttonCatalogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+           catalogoFragment = new CatalogoFragment();
+
+           FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+           transaction.replace(R.id.frameConteudo, catalogoFragment);
+           transaction.commit();
+            }
+        });
+
+        buttonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                homeFragment = new HomeFragment();
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frameConteudo, homeFragment);
+                transaction.commit();
+            }
+        });
+
+        buttonMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menuFragment = new MenuFragment();
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frameConteudo, menuFragment);
+                transaction.commit();
+
+            }
+        });
+
+    }
+}
